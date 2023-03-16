45f4444d426245697389869186fd5069acd8bc47
Sofía <sofia.neira@ias.com.com>
2023-03-16 14:36:38 -0500

    config server

---
+++ ConfigService/src/test/java/com/config/ConfigService/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.ConfigService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

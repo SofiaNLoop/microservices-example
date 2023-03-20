483fd038623beb5e5f00af373ea26fd1f8b005d6
Sofía <sofia.neira@ias.com.com>
2023-03-20 16:35:55 -0500

    eureka config

--- ExampMicro/src/main/java/Microservicios/ExampMicro/ExampMicroApplication.java
+++ ExampMicro/src/main/java/Microservicios/ExampMicro/ExampMicroApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+
 public class ExampMicroApplication {
 
 	public static void main(String[] args) {
